cca4971d8a4fcfb8279f85b056b0b123750b49eb
Harry-Kachigamba <harrykachigamba22@outlook.com>
2024-10-29 18:38:03 +0200

    Initial commit

---
+++ reactjsAppwriteAuthentication/src/lib/appwrite.js
@@ -0,0 +1,10 @@
+import { Client, Account } from 'appwrite';
+
+export const client = new Client();
+
+client
+    .setEndpoint('')
+    .setProject('');
+
+export const account = new Account(client);
+export { ID } from 'appwrite';
